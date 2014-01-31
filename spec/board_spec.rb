03ba4dc2bb3672b489f9d1bc1bb96f7722a8acbb
Rich Dammkoehler <rpd@noradltd.com>
2014-01-31 13:11:18 -0600

    a little more code clean up

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,39 +3,40 @@ require 'spec_helper'
 
 describe Board, "#get_in_progress_total_size" do
 
+  let(:empty_board) { Board.new([], []) }
+  let(:in_progress_card) { Card.new(1) }
+  let(:in_progress_cards) { [in_progress_card, in_progress_card, in_progress_card] }
+  let(:completed_cards) { [] }
+
   it 'should return 0 if no cards in progress' do
-    b = Board.new([], [])
-    b.get_in_progress_total_size.should eq 0
+    empty_board.get_in_progress_total_size.should eq 0
   end
 
-  it 'should return 1 if one card in progress with size of 1' do
-    in_progress_card = Card.new(1)
-    b = Board.new([in_progress_card], [])
-    b.get_in_progress_total_size.should eq 1
+  it 'should return 1 if one card in progress with size of 1' do  
+    Board.new([in_progress_card], completed_cards).get_in_progress_total_size.should eq 1
   end
 
   it 'should return 3 if three cards in progress with size of 1' do
-    in_progress_card = Card.new(1)
-    b = Board.new([in_progress_card, in_progress_card, in_progress_card], [])
-    b.get_in_progress_total_size.should eq 3
+    Board.new(in_progress_cards, completed_cards).get_in_progress_total_size.should eq 3
   end
 end
 
 describe Board, "#get_complete_total_size" do
+
+  let(:empty_board) { Board.new([], []) }
+  let(:complete_card) { Card.new(2) }
+  let(:in_progress_cards) { [] }
+  let(:completed_cards) { [complete_card, complete_card, complete_card] }
+
   it 'should return 0 if no cards in progress' do
-    b = Board.new([],[])
-    b.get_complete_total_size.should eq 0
+    empty_board.get_complete_total_size.should eq 0
   end
 
   it 'should return 2 if one card in progress with size of 2' do
-    complete_card = Card.new(2)
-    b = Board.new([], [complete_card])
-    b.get_complete_total_size.should eq 2
+    Board.new(in_progress_cards, [complete_card]).get_complete_total_size.should eq 2
   end
 
   it 'should return 6 if three cards in progress with size of 2' do
-    complete_card = Card.new(2)
-    b = Board.new([], [complete_card, complete_card, complete_card])
-    b.get_complete_total_size.should eq 6
+    Board.new(in_progress_cards, completed_cards).get_complete_total_size.should eq 6
   end
 end
