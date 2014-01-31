fa55ed5fd58511933b94237102a4bdc7bf2b9d12
Rich Dammkoehler <rpd@noradltd.com>
2014-01-31 13:17:34 -0600

    minor cleanup, use let not before.each

--- spec/trello_extractor_spec.rb
+++ spec/trello_extractor_spec.rb
@@ -31,32 +31,31 @@ describe TrelloExtractor, '#load_board' do
 end
 
 describe TrelloExtractor, '#get_size' do
-  before(:each) do
-    @trello_extractor = TrelloExtractor.new()
-  end
+  
+  let(:trello_extractor) { TrelloExtractor.new() }
 
   it 'should return one when one is in the name' do
     foo_card = TrelloHelper.build_card_with_size('Foo', 1)
-    @trello_extractor.get_size(foo_card.name).should eq 1
+    trello_extractor.get_size(foo_card.name).should eq 1
   end
 
   it 'should return -1 when no size in the card name' do
     card = TrelloHelper.build_card('foo')
-    @trello_extractor.get_size(card.name).should eq -1
+    trello_extractor.get_size(card.name).should eq -1
   end
 
   it 'should return 2 when {2} is in the card name' do
     card = TrelloHelper.build_card('Foo {2}')
-    @trello_extractor.get_size(card.name).should eq 2
+    trello_extractor.get_size(card.name).should eq 2
   end
 
   it 'should return 3 when numbers exist in card name' do
     card = TrelloHelper.build_card('Foo 4 5 2 <3>')
-    @trello_extractor.get_size(card.name).should eq 3
+    trello_extractor.get_size(card.name).should eq 3
   end
 
   it 'should return 4 when numbers before and after size' do
     card = TrelloHelper.build_card('Foo 3 2 [4] 23')
-    @trello_extractor.get_size(card.name).should eq 4
+    trello_extractor.get_size(card.name).should eq 4
   end
 end
