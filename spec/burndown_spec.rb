6ab033a9280f45d69589fe9d7b5daeff4c4d70b8
Rich Dammkoehler <rpd@noradltd.com>
2014-01-31 13:00:10 -0600

    some basic clean up of the code

--- spec/burndown_spec.rb
+++ spec/burndown_spec.rb
@@ -1,75 +1,59 @@
 require 'rspec'
 require 'spec_helper'
-
+  
 describe Burndown, "#get_current_csv_output" do
 
-  it 'populated board should output csv burndown' do
-    in_progress_card = Card.new(1)
-    completed_card = Card.new(1)
-    board = Board.new([in_progress_card], [completed_card])
-
-    @time_now = Time.parse("Jan 18 2014")
+  let(:in_progress_card) { Card.new(1) }
+  let(:completed_card) { Card.new(1) }
+  let(:board) { Board.new([in_progress_card], [completed_card]) }
+  let(:mock_time) {
+    @time_now = Time.parse("Jan 18 2014 00:00:00 -0600")
     Time.stub(:now).and_return(@time_now)
     mock_time = Time.now
+  }
 
-    burndown = Burndown.new(board)
+  it 'populated board should output csv burndown' do
     expected_output = '"%s",%d,%d' % [mock_time, 1,1]
-    burndown.get_current_csv_output.should eq expected_output
+    Burndown.new(board).get_current_csv_output.should eq expected_output
   end
 end
 
 describe Burndown, "#write_burndown_to_file", fakefs: true do
+
+  let(:in_progress_card) { Card.new(1) }
+  let(:completed_card) { Card.new(1) }
+  let(:board) { Board.new([in_progress_card], [completed_card]) }
+  let(:empty_board) { Board.new([],[]) } 
+  let(:mock_time) {
+    @time_now = Time.parse("Jan 18 2014 00:00:00 -0600")
+    Time.stub(:now).and_return(@time_now)
+    mock_time = Time.now
+  }
+  let(:burndown_file) { "out.csv" }
+
   it 'will write the header if output file does not exist' do
-    board = Board.new([],[])
-
-    burndown = Burndown.new(board)
-    burndown.write_burndown_to_file("out.csv")
-
-    File.read("out.csv").should start_with '"Reported","Doing","Done"'
+    Burndown.new(empty_board).write_burndown_to_file(burndown_file)
+    File.read(burndown_file).should start_with '"Reported","Doing","Done"'
   end
 
   it 'will not write the header if output file exists' do
-    board = Board.new([],[])
-
-    FileUtils.touch("out.csv")
-
-    burndown = Burndown.new(board)
-    burndown.write_burndown_to_file("out.csv")
-
-    File.read("out.csv").should_not start_with '"Reported","Doing","Done"'
+    FileUtils.touch(burndown_file)
+    Burndown.new(empty_board).write_burndown_to_file(burndown_file)
+    File.read(burndown_file).should_not start_with '"Reported","Doing","Done"'
   end
 
   it 'will write the current csv output' do
-    in_progress_card = Card.new(1)
-    completed_card = Card.new(1)
-    board = Board.new([in_progress_card], [completed_card])
-
-    @time_now = Time.parse("Jan 18 2014")
-    Time.stub(:now).and_return(@time_now)
-    mock_time = Time.now
-
-    burndown = Burndown.new(board)
     expected_output = '"%s",%d,%d' % [mock_time, 1,1]
-    burndown.write_burndown_to_file("out.csv")
-    File.read("out.csv").should include expected_output
+    Burndown.new(board).write_burndown_to_file(burndown_file)
+    File.read(burndown_file).should include expected_output
   end
 
   it 'will append current csv output to end of existing file' do
     File.open("out.csv", 'w') do |f|
       f.puts("Initial")
     end
-
-    in_progress_card = Card.new(1)
-    completed_card = Card.new(1)
-    board = Board.new([in_progress_card], [completed_card])
-
-    @time_now = Time.parse("Jan 18 2014")
-    Time.stub(:now).and_return(@time_now)
-    mock_time = Time.now
-
-    burndown = Burndown.new(board)
     expected_output = "Initial\n\"%s\",%d,%d\n" % [mock_time, 1,1]
-    burndown.write_burndown_to_file("out.csv")
-    File.read("out.csv").should eq expected_output
+    Burndown.new(board).write_burndown_to_file(burndown_file)
+    File.read(burndown_file).should eq expected_output
   end
 end
