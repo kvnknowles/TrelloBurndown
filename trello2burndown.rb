c5c85326177e5cb9fae0f19ab66f6f4443062253
Kevin Knowles <knowles.kevin@gmail.com>
2014-02-09 14:28:15 -0600

    Add option to set output file

--- trello2burndown.rb
+++ trello2burndown.rb
@@ -3,7 +3,7 @@ require_relative 'lib/trello_extractor'
 require_relative 'lib/burndown'
 
 opts = Trollop::options do
-  version = "trello2burndown 0.0.1 (c) 2013 Bogotech Inc"
+  version = 'trello2burndown 0.0.1'
   banner <<-EOS
 trello2burndown converts a trello board into a csv
 
@@ -11,8 +11,9 @@ Usage:
   ruby trello2burndown.rb [options]
 where [options] are:
   EOS
-  opt :board_name, "Board Name", :type => String, :required => true
+  opt :board_name, 'Board Name', :type => String, :required => true
+  opt :output_file, 'Output File', :type => String, :default => 'out.csv'
 end
 
 board = TrelloExtractor.new().load_board(opts[:board_name])
-Burndown.new(board).write_burndown_to_file('out.csv')
+Burndown.new(board).write_burndown_to_file(opts[:output_file])
