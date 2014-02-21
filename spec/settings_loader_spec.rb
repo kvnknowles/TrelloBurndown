2d8a1b4094090961c4239c9e3e416043a3033fbb
Rich Dammkoehler <rpd@noradltd.com>
2014-02-21 14:46:06 -0600

    replaced constant with let statement

--- spec/settings_loader_spec.rb
+++ spec/settings_loader_spec.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'fakefs/safe'
 
 describe SettingsLoader, '#load_sizes' do
-  TEST_SETTINGS_FILE = 'settings.yml'
+  let(:test_settings_file) {'settings.yml' }
   let(:default_sizes) { Hash[:default, 0, :small, 1, :medium, 3, :large, 5] }
 
   it 'should return a hash of sizes if yaml has sizes' do
@@ -11,18 +11,18 @@ describe SettingsLoader, '#load_sizes' do
     parsed_yaml = {
         'sizes' => expectedHash
     }
-    YAML.should_receive(:load_file).with(TEST_SETTINGS_FILE).and_return parsed_yaml
-    SettingsLoader.load_sizes(TEST_SETTINGS_FILE).should eq expectedHash
+    YAML.should_receive(:load_file).with(test_settings_file).and_return parsed_yaml
+    SettingsLoader.load_sizes(test_settings_file).should eq expectedHash
   end
 
   it 'should return default structure if yaml does not have sizes' do
-    YAML.should_receive(:load_file).with(TEST_SETTINGS_FILE).and_return Hash.new()
-    SettingsLoader.load_sizes(TEST_SETTINGS_FILE).should include default_sizes
+    YAML.should_receive(:load_file).with(test_settings_file).and_return Hash.new()
+    SettingsLoader.load_sizes(test_settings_file).should include default_sizes
   end
 
   it 'should return default structure if file does not exist' do
     FakeFS do
-      SettingsLoader.load_sizes(TEST_SETTINGS_FILE).should include default_sizes
+      SettingsLoader.load_sizes(test_settings_file).should include default_sizes
     end
   end
 
@@ -32,8 +32,8 @@ describe SettingsLoader, '#load_sizes' do
         'sizes' => expectedHash,
         'default' => 5
     }
-    YAML.should_receive(:load_file).with(TEST_SETTINGS_FILE).and_return parsed_yaml
-    SettingsLoader.load_sizes(TEST_SETTINGS_FILE)[:default].should eq 5
+    YAML.should_receive(:load_file).with(test_settings_file).and_return parsed_yaml
+    SettingsLoader.load_sizes(test_settings_file)[:default].should eq 5
   end
 
   it 'sets default size to 0 if yaml file does not set default' do
@@ -41,7 +41,7 @@ describe SettingsLoader, '#load_sizes' do
     parsed_yaml = {
         'sizes' => expectedHash
     }
-    YAML.should_receive(:load_file).with(TEST_SETTINGS_FILE).and_return parsed_yaml
-    SettingsLoader.load_sizes(TEST_SETTINGS_FILE)[:default].should eq 0
+    YAML.should_receive(:load_file).with(test_settings_file).and_return parsed_yaml
+    SettingsLoader.load_sizes(test_settings_file)[:default].should eq 0
   end
 end
